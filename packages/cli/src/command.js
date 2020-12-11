0a1d42f0e7dd13499de9df702c0382dd10b814d3
dashixiong <876337269@qq.com>
2020-12-11 15:03:28 +0800

    chore: 删除了一些不用的文件

--- packages/cli/src/command.js
+++ packages/cli/src/command.js
@@ -1,239 +1,221 @@
-
-import path from 'path'
-import consola from 'consola'
-import minimist from 'minimist'
-import Hookable from 'hable'
-import { name, version } from '../package.json'
-import { forceExit } from './utils'
-import { loadNuxtConfig } from './utils/config'
-import { indent, foldLines, colorize } from './utils/formatting'
-import { startSpaces, optionSpaces, forceExitTimeout } from './utils/constants'
-import * as imports from './imports'
+import path from "path";
+import consola from "consola";
+import minimist from "minimist";
+import Hookable from "hable";
+import { name, version } from "../package.json";
+import { forceExit } from "./utils";
+import { loadNuxtConfig } from "./utils/config";
+import { indent, foldLines, colorize } from "./utils/formatting";
+import { startSpaces, optionSpaces, forceExitTimeout } from "./utils/constants";
+import * as imports from "./imports";
 
 export default class NuxtCommand extends Hookable {
-  constructor (cmd = { name: '', usage: '', description: '' }, argv = process.argv.slice(2), hooks = {}) {
-    super(consola)
-    this.addHooks(hooks)
+  constructor(
+    cmd = { name: "", usage: "", description: "" },
+    argv = process.argv.slice(2),
+    hooks = {}
+  ) {
+    super(consola);
+    this.addHooks(hooks);
 
     if (!cmd.options) {
-      cmd.options = {}
+      cmd.options = {};
     }
-    this.cmd = cmd
+    this.cmd = cmd;
 
-    this._argv = Array.from(argv)
-    this._parsedArgv = null // Lazy evaluate
+    this._argv = Array.from(argv);
+    this._parsedArgv = null; // Lazy evaluate
   }
 
-  static run (cmd, argv, hooks) {
-    return NuxtCommand.from(cmd, argv, hooks).run()
+  static run(cmd, argv, hooks) {
+    return NuxtCommand.from(cmd, argv, hooks).run();
   }
 
-  static from (cmd, argv, hooks) {
+  static from(cmd, argv, hooks) {
     if (cmd instanceof NuxtCommand) {
-      return cmd
+      return cmd;
     }
-    return new NuxtCommand(cmd, argv, hooks)
+    return new NuxtCommand(cmd, argv, hooks);
   }
 
-  async run () {
-    await this.callHook('run:before', {
+  async run() {
+    await this.callHook("run:before", {
       argv: this._argv,
       cmd: this.cmd,
-      rootDir: path.resolve(this.argv._[0] || '.')
-    })
+      rootDir: path.resolve(this.argv._[0] || ".")
+    });
 
     if (this.argv.help) {
-      this.showHelp()
-      return
+      this.showHelp();
+      return;
     }
 
     if (this.argv.version) {
-      this.showVersion()
-      return
+      this.showVersion();
+      return;
     }
 
-    if (typeof this.cmd.run !== 'function') {
-      throw new TypeError('Invalid command! Commands should at least implement run() function.')
+    if (typeof this.cmd.run !== "function") {
+      throw new TypeError(
+        "Invalid command! Commands should at least implement run() function."
+      );
     }
 
-    let cmdError
+    let cmdError;
 
     try {
-      await this.cmd.run(this)
+      await this.cmd.run(this);
     } catch (e) {
-      cmdError = e
-    }
-
-    if (this.argv.lock) {
-      await this.releaseLock()
-    }
-
-    if (this.argv['force-exit']) {
-      const forceExitByUser = this.isUserSuppliedArg('force-exit')
-      if (cmdError) {
-        consola.fatal(cmdError)
-      }
-      forceExit(this.cmd.name, forceExitByUser ? false : forceExitTimeout)
-      if (forceExitByUser) {
-        return
-      }
-    }
-
-    if (cmdError) {
-      throw cmdError
+      cmdError = e;
     }
   }
 
-  showVersion () {
-    process.stdout.write(`${name} v${version}\n`)
+  showVersion() {
+    process.stdout.write(`${name} v${version}\n`);
   }
 
-  showHelp () {
-    process.stdout.write(this._getHelp())
+  showHelp() {
+    process.stdout.write(this._getHelp());
   }
 
-  get argv () {
+  get argv() {
     if (!this._parsedArgv) {
-      const minimistOptions = this._getMinimistOptions()
-      this._parsedArgv = minimist(this._argv, minimistOptions)
+      const minimistOptions = this._getMinimistOptions();
+      this._parsedArgv = minimist(this._argv, minimistOptions);
     }
-    return this._parsedArgv
+    return this._parsedArgv;
   }
 
-  async getNuxtConfig (extraOptions = {}) {
+  async getNuxtConfig(extraOptions = {}) {
     // Flag to indicate nuxt is running with CLI (not programmatic)
-    extraOptions._cli = true
+    extraOptions._cli = true;
 
     const context = {
       command: this.cmd.name,
       dev: !!extraOptions.dev
-    }
+    };
 
-    const config = await loadNuxtConfig(this.argv, context)
-    const options = Object.assign(config, extraOptions)
+    const config = await loadNuxtConfig(this.argv, context);
+    const options = Object.assign(config, extraOptions);
 
     for (const name of Object.keys(this.cmd.options)) {
-      this.cmd.options[name].prepare && this.cmd.options[name].prepare(this, options, this.argv)
+      this.cmd.options[name].prepare &&
+        this.cmd.options[name].prepare(this, options, this.argv);
     }
 
-    await this.callHook('config', options)
+    await this.callHook("config", options);
 
-    return options
+    return options;
   }
 
-  async getNuxt (options) {
-    const { Nuxt } = await imports.core()
+  async getNuxt(options) {
+    const { Nuxt } = await imports.core();
 
-    const nuxt = new Nuxt(options)
-    await nuxt.ready()
+    const nuxt = new Nuxt(options);
+    await nuxt.ready();
 
-    return nuxt
+    return nuxt;
   }
 
-  async getBuilder (nuxt) {
-    const { Builder } = await imports.builder()
-    const { BundleBuilder } = await imports.webpack()
-    return new Builder(nuxt, BundleBuilder)
+  async getBuilder(nuxt) {
+    const { Builder } = await imports.builder();
+    const { BundleBuilder } = await imports.webpack();
+    return new Builder(nuxt, BundleBuilder);
   }
 
-  async getGenerator (nuxt) {
-    const { Generator } = await imports.generator()
-    const builder = await this.getBuilder(nuxt)
-    return new Generator(nuxt, builder)
+  async getGenerator(nuxt) {
+    const { Generator } = await imports.generator();
+    const builder = await this.getBuilder(nuxt);
+    return new Generator(nuxt, builder);
   }
 
-  async setLock (lockRelease) {
-    if (lockRelease) {
-      if (this._lockRelease) {
-        consola.warn(`A previous unreleased lock was found, this shouldn't happen and is probably an error in 'nuxt ${this.cmd.name}' command. The lock will be removed but be aware of potential strange results`)
-
-        await this.releaseLock()
-        this._lockRelease = lockRelease
-      } else {
-        this._lockRelease = lockRelease
-      }
-    }
+  isUserSuppliedArg(option) {
+    return (
+      this._argv.includes(`--${option}`) ||
+      this._argv.includes(`--no-${option}`)
+    );
   }
 
-  async releaseLock () {
-    if (this._lockRelease) {
-      await this._lockRelease()
-      this._lockRelease = undefined
-    }
+  _getDefaultOptionValue(option) {
+    return typeof option.default === "function"
+      ? option.default(this.cmd)
+      : option.default;
   }
 
-  isUserSuppliedArg (option) {
-    return this._argv.includes(`--${option}`) || this._argv.includes(`--no-${option}`)
-  }
-
-  _getDefaultOptionValue (option) {
-    return typeof option.default === 'function' ? option.default(this.cmd) : option.default
-  }
-
-  _getMinimistOptions () {
+  _getMinimistOptions() {
     const minimistOptions = {
       alias: {},
       boolean: [],
       string: [],
       default: {}
-    }
+    };
 
     for (const name of Object.keys(this.cmd.options)) {
-      const option = this.cmd.options[name]
+      const option = this.cmd.options[name];
 
       if (option.alias) {
-        minimistOptions.alias[option.alias] = name
+        minimistOptions.alias[option.alias] = name;
       }
       if (option.type) {
-        minimistOptions[option.type].push(option.alias || name)
+        minimistOptions[option.type].push(option.alias || name);
       }
       if (option.default) {
-        minimistOptions.default[option.alias || name] = this._getDefaultOptionValue(option)
+        minimistOptions.default[
+          option.alias || name
+        ] = this._getDefaultOptionValue(option);
       }
     }
 
-    return minimistOptions
+    return minimistOptions;
   }
 
-  _getHelp () {
-    const options = []
-    let maxOptionLength = 0
+  _getHelp() {
+    const options = [];
+    let maxOptionLength = 0;
 
     for (const name in this.cmd.options) {
-      const option = this.cmd.options[name]
+      const option = this.cmd.options[name];
 
-      let optionHelp = '--'
-      optionHelp += option.type === 'boolean' && this._getDefaultOptionValue(option) ? 'no-' : ''
-      optionHelp += name
+      let optionHelp = "--";
+      optionHelp +=
+        option.type === "boolean" && this._getDefaultOptionValue(option)
+          ? "no-"
+          : "";
+      optionHelp += name;
       if (option.alias) {
-        optionHelp += `, -${option.alias}`
+        optionHelp += `, -${option.alias}`;
       }
 
-      maxOptionLength = Math.max(maxOptionLength, optionHelp.length)
-      options.push([optionHelp, option.description])
+      maxOptionLength = Math.max(maxOptionLength, optionHelp.length);
+      options.push([optionHelp, option.description]);
     }
 
-    const _opts = options.map(([option, description]) => {
-      const i = indent(maxOptionLength + optionSpaces - option.length)
-      return foldLines(
-        option + i + description,
-        startSpaces + maxOptionLength + optionSpaces * 2,
-        startSpaces + optionSpaces
-      )
-    }).join('\n')
+    const _opts = options
+      .map(([option, description]) => {
+        const i = indent(maxOptionLength + optionSpaces - option.length);
+        return foldLines(
+          option + i + description,
+          startSpaces + maxOptionLength + optionSpaces * 2,
+          startSpaces + optionSpaces
+        );
+      })
+      .join("\n");
 
-    const usage = foldLines(`Usage: nuxt ${this.cmd.usage} [options]`, startSpaces)
-    const description = foldLines(this.cmd.description, startSpaces)
-    const opts = foldLines('Options:', startSpaces) + '\n\n' + _opts
+    const usage = foldLines(
+      `Usage: nuxt ${this.cmd.usage} [options]`,
+      startSpaces
+    );
+    const description = foldLines(this.cmd.description, startSpaces);
+    const opts = foldLines("Options:", startSpaces) + "\n\n" + _opts;
 
-    let helpText = colorize(`${usage}\n\n`)
+    let helpText = colorize(`${usage}\n\n`);
     if (this.cmd.description) {
-      helpText += colorize(`${description}\n\n`)
+      helpText += colorize(`${description}\n\n`);
     }
     if (options.length) {
-      helpText += colorize(`${opts}\n\n`)
+      helpText += colorize(`${opts}\n\n`);
     }
 
-    return helpText
+    return helpText;
   }
 }
--- packages/webpack/src/config/server.js
+++ packages/webpack/src/config/server.js
@@ -1,33 +1,27 @@
-import path from 'path'
-import fs from 'fs'
-import { DefinePlugin, ProvidePlugin } from 'webpack'
-import nodeExternals from 'webpack-node-externals'
+import path from "path";
+import fs from "fs";
+import { DefinePlugin, ProvidePlugin } from "webpack";
+import nodeExternals from "webpack-node-externals";
 
-import VueSSRServerPlugin from '../plugins/vue/server'
+import VueSSRServerPlugin from "../plugins/vue/server";
 
-import WebpackBaseConfig from './base'
+import WebpackBaseConfig from "./base";
 
-const nativeFileExtensions = [
-  '.json',
-  '.js'
-]
+const nativeFileExtensions = [".json", ".js"];
 
 export default class WebpackServerConfig extends WebpackBaseConfig {
-  constructor (...args) {
-    super(...args)
-    this.name = 'server'
-    this.isServer = true
+  constructor(...args) {
+    super(...args);
+    this.name = "server";
+    this.isServer = true;
   }
 
-  get devtool () {
-    return 'cheap-module-source-map'
+  get devtool() {
+    return "cheap-module-source-map";
   }
 
-  get externalsAllowlist () {
-    return [
-      this.isNonNativeImport.bind(this),
-      ...this.normalizeTranspile()
-    ]
+  get externalsAllowlist() {
+    return [this.isNonNativeImport.bind(this), ...this.normalizeTranspile()];
   }
 
   /**
@@ -37,86 +31,85 @@ export default class WebpackServerConfig extends WebpackBaseConfig {
    * - "someFile.umd" (actually requiring someFile.umd.js)
    * - "some.folder" (some.folder being a directory containing a package.json)
    */
-  isNonNativeImport (modulePath) {
-    const extname = path.extname(modulePath)
-    return extname !== '' && !nativeFileExtensions.includes(extname)
+  isNonNativeImport(modulePath) {
+    const extname = path.extname(modulePath);
+    return extname !== "" && !nativeFileExtensions.includes(extname);
   }
 
-  env () {
-    return Object.assign(
-      super.env(),
-      {
-        'process.env.VUE_ENV': JSON.stringify('server'),
-        'process.browser': false,
-        'process.client': false,
-        'process.server': true,
-        'process.modern': false
-      }
-    )
+  env() {
+    return Object.assign(super.env(), {
+      "process.env.VUE_ENV": JSON.stringify("server"),
+      "process.browser": false,
+      "process.client": false,
+      "process.server": true,
+      "process.modern": false
+    });
   }
 
-  optimization () {
-    const { _minifyServer } = this.buildContext.buildOptions
+  optimization() {
+    const { _minifyServer } = this.buildContext.buildOptions;
 
     return {
       splitChunks: false,
       minimizer: _minifyServer ? this.minimizer() : []
-    }
+    };
   }
 
-  resolve () {
-    const resolveConfig = super.resolve()
+  resolve() {
+    const resolveConfig = super.resolve();
 
-    resolveConfig.resolve.mainFields = ['main', 'module']
+    resolveConfig.resolve.mainFields = ["main", "module"];
 
-    return resolveConfig
+    return resolveConfig;
   }
 
-  alias () {
-    const aliases = super.alias()
+  alias() {
+    const aliases = super.alias();
 
     for (const p of this.buildContext.plugins) {
       if (!aliases[p.name]) {
         // Do not load client-side plugins on server-side
-        aliases[p.name] = p.mode === 'client' ? './empty.js' : p.src
+        aliases[p.name] = p.mode === "client" ? "./empty.js" : p.src;
       }
     }
 
-    return aliases
+    return aliases;
   }
 
-  plugins () {
-    const plugins = super.plugins()
+  plugins() {
+    const plugins = super.plugins();
     plugins.push(
       new VueSSRServerPlugin({ filename: `${this.name}.manifest.json` }),
       new DefinePlugin(this.env())
-    )
+    );
 
-    const { serverURLPolyfill } = this.buildContext.options.build
+    const { serverURLPolyfill } = this.buildContext.options.build;
 
     if (serverURLPolyfill) {
-      plugins.push(new ProvidePlugin({
-        URL: [serverURLPolyfill, 'URL'],
-        URLSearchParams: [serverURLPolyfill, 'URLSearchParams']
-      }))
+      plugins.push(
+        new ProvidePlugin({
+          URL: [serverURLPolyfill, "URL"],
+          URLSearchParams: [serverURLPolyfill, "URLSearchParams"]
+        })
+      );
     }
 
-    return plugins
+    return plugins;
   }
 
-  config () {
-    const config = super.config()
+  config() {
+    const config = super.config();
 
     Object.assign(config, {
-      target: 'node',
+      target: "node",
       node: false,
       entry: Object.assign({}, config.entry, {
-        app: [path.resolve(this.buildContext.options.buildDir, 'server.js')]
+        app: [path.resolve(this.buildContext.options.buildDir, "server.js")]
       }),
       output: Object.assign({}, config.output, {
-        filename: 'server.js',
-        chunkFilename: '[name].js',
-        libraryTarget: 'commonjs2'
+        filename: "server.js",
+        chunkFilename: "[name].js",
+        libraryTarget: "commonjs2"
       }),
       performance: {
         hints: false,
@@ -124,24 +117,24 @@ export default class WebpackServerConfig extends WebpackBaseConfig {
         maxAssetSize: Infinity
       },
       externals: [].concat(config.externals || [])
-    })
+    });
 
     // https://webpack.js.org/configuration/externals/#externals
     // https://github.com/liady/webpack-node-externals
     // https://vue-loader.vuejs.org/migrating.html#ssr-externals
     if (!this.buildContext.buildOptions.standalone) {
-      this.buildContext.options.modulesDir.forEach((dir) => {
+      this.buildContext.options.modulesDir.forEach(dir => {
         if (fs.existsSync(dir)) {
           config.externals.push(
             nodeExternals({
               allowlist: this.externalsAllowlist,
               modulesDir: dir
             })
-          )
+          );
         }
-      })
+      });
     }
 
-    return config
+    return config;
   }
 }
